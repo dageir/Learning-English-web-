e81ee54ffe823943aac5fcdd28849382f17e38af
dageir <laner@gmail.com>
2022-01-23 19:52:39 +0300

    add test v=1.5

--- first_site_v1.2/education/test_one/models.py
+++ first_site_v1.2/education/test_one/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+#Первый вариант модели тестирования (старый вариант)
 class Exam(models.Model):
     question = models.CharField('Вопрос', max_length=250)
     answers = models.TextField('Все возможные ответы на вопросы')
@@ -14,11 +14,11 @@ class Exam(models.Model):
         answer = str(self.answers).split(key_ans)
         return answer
 
-
     class Meta:
         verbose_name = 'Экзамен'
         verbose_name_plural = 'Экзамены'
 
+#Модель тестирования (второй вариант, с ограниченным кол-вом ответов)
 class TestingWithChangeAnswer(models.Model):
 
     question = models.TextField('Вопрос')
@@ -34,3 +34,4 @@ class TestingWithChangeAnswer(models.Model):
     class Meta:
         verbose_name = 'Тестирование'
         verbose_name_plural = 'Тестирования'
+
